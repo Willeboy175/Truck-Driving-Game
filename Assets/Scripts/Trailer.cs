793c3a28d57e7888c2e837f85fb08865cfe3aea4
Willeboy175 <william.strandberg@elev.ga.lbs.se>
2024-09-04 17:43:30 +0200

    Truck and trailer stable

--- Assets/Scripts/Trailer.cs
+++ Assets/Scripts/Trailer.cs
@@ -6,6 +6,11 @@ public class Trailer : MonoBehaviour
 {
     public Transform com;
     public Rigidbody rb;
+    public Transform drawbar;
+    public Transform dolly;
+    public WheelCollider[] wheels;
+
+    private Vector3 targetRotation;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +22,16 @@ public class Trailer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        targetRotation = drawbar.localEulerAngles;
+
+        targetRotation.x = 0;
+        targetRotation.z = 0;
+
+        dolly.localEulerAngles = targetRotation;
+
+        for (int i = 0; i < wheels.Length; i++)
+        {
+            wheels[i].steerAngle = targetRotation.y;
+        }
     }
 }
